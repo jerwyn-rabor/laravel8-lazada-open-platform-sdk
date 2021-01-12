8e60cc8e1a7ceea9958ca0951bbf13dbb35d6e13
Jerwyn Rabor <jrabor@i4asiacorp.com>
2021-01-12 17:30:52 +0800

    Create array manipulation format for Lazada data

--- app/Http/Controllers/LazadaController.php
+++ app/Http/Controllers/LazadaController.php
@@ -14,7 +14,7 @@ class LazadaController extends Controller
         $appSecret = config('lazada.api.appsecret');
         $accessToken = config('lazada.api.accesstoken');
 
-        // LAZADA OPEN PLATFORM SDK
+        // LAZADA OPEN PLATFORM SDK CODE
         $c = new LazopClient($url,$appkey,$appSecret);
         $request = new LazopRequest('/products/get','GET');
         $request->addApiParam('filter','live');
@@ -25,7 +25,34 @@ class LazadaController extends Controller
         // CONVERT STRING TO JSON DATA
         $result = json_decode($c->execute($request, $accessToken), true);
 
-        return $result;
+        // PRE-FORMAT LAZADA DATA
+        $products = collect($result["data"]["products"]) ?? [];
 
+        return $this->prepGoogleSheetData($products);
+    }
+
+    public function prepGoogleSheetData($products) {
+
+        $values = [];
+
+        foreach($products as $product) {
+
+            // IF YOU WANT TO SEE THE STRUCTURE OF DATA
+            // dd($product);
+
+            $temp = [];
+            $temp[] = $product["attributes"]["name"] ?? '';
+            $temp[] = $product["attributes"]["short_description"] ?? '';
+            $temp[] = $product["attributes"]["description"] ?? '';
+            $temp[] = $product["attributes"]["material_filter"] ?? '';
+            $temp[] = $product["attributes"]["fa_pattern"] ?? '';
+            $temp[] = $product["attributes"]["brand"] ?? '';
+            $temp[] = $product["attributes"]["warranty_type"] ?? '';
+
+            // PUSH FORMAT TO VALUES
+            $values[] = $temp;
+        };
+        
+        return $values;
     }
 }
